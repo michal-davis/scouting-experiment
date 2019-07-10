0a52d52d69a47c11e73a6f52f63fdb57d1472f2c
Jim Davis <jrd3@alum.mit.edu>
2019-07-10 00:24:14 -0400

    load_matches now exits gracefully after all loaded

--- load_matches.js
+++ load_matches.js
@@ -8,13 +8,34 @@ const sequentially = require('./sequentially.js');
 // Using connection to database, add the match object
 //basic match schedule information is stored in several tables: matches, alliance, and alliance_member
 
-function add_matches(connection, match) {
-    console.log(match.event_key + " " + match.match_number);
+var matches = [];
 
-    var alliance_ids = {"red": null, "blue": null};
+// collect all the matches into a list.
+function add_match(connection, match) {
+	matches.push(match);
+}
+
+// this is called after TBA has found all the matches.
+function after_all_matches_are_collected_load_them(connection) {
+	console.log("Loading " + matches.length + " matches");
+	load_match_list(connection, matches, () => 	connection.end());
+}
+
+// load the list matches, one at a time.
+// when all are loaded, call the done function.
+function load_match_list(connection, matches, done) {
+	if (matches.length == 0) {
+		done();
+		return;
+	}
+
+	var match = matches.pop();
+	console.log(match.event_key + " " + match.match_number);
+	var alliance_ids = {"red": null, "blue": null};
 
 	// Node runs each database query asynchronously, but we want to wait for each
-	// query's callback to return before doing the next one
+	// query's callback to return before doing the next one so we can use results
+	// from previous queries
 
 	sequentially.do_things(connection, [
 		{
@@ -23,12 +44,16 @@ function add_matches(connection, match) {
                 " ON DUPLICATE KEY UPDATE event_code = event_code",
 			args: [match.match_number, match.event_key]
 		},
+
+		// create red alliance
 		{
 			query: "INSERT INTO alliance (match_number, event_code, practice, alliance_colour)" +
                 " VALUES (?, ?, FALSE, ?)" + 
                 "ON DUPLICATE KEY UPDATE event_code = event_code",
 			args: [match.match_number, match.event_key, 'red']
 		},
+
+		// get the id of that alliance
 		{
 			query: "SELECT id FROM alliance" +
 				" WHERE match_number = ?" +
@@ -38,22 +63,10 @@ function add_matches(connection, match) {
 			args: [match.match_number, match.event_key, false, 'red'],
 			h: results => alliance_ids["red"] = results[0].id
 		},
-		{
-			query: "INSERT INTO alliance (match_number, event_code, practice, alliance_colour)" +
-                " VALUES (?, ?, FALSE, ?)" + 
-				" ON DUPLICATE KEY UPDATE event_code = event_code",
-			args: [match.match_number, match.event_key, 'blue']
-		},
-		{
-			query: "SELECT id FROM alliance" +
-				" WHERE match_number = ?" +
-				"   AND event_code = ?" +
-				"   AND practice = ?" +
-				"   AND alliance_colour = ?",
-			args: [match.match_number, match.event_key, false, "blue"],
-			h: results => alliance_ids["blue"] = results[0].id
-		},
 
+		// insert the red members 0,1,2
+		// note that we need to use a function to get the args for the query
+		// because we do not know the alliance id until after the previous query
 		{
 			query: "INSERT INTO alliance_member (alliance_id, team_number)" +
 				" VALUES (?, ?) " +
@@ -78,6 +91,24 @@ function add_matches(connection, match) {
 						 parseInt(match.alliances["red"].team_keys[2].substring(3), 10)]
 		},
 
+		// insert the blue alliance and its member teams
+
+		{
+			query: "INSERT INTO alliance (match_number, event_code, practice, alliance_colour)" +
+                " VALUES (?, ?, FALSE, ?)" + 
+				" ON DUPLICATE KEY UPDATE event_code = event_code",
+			args: [match.match_number, match.event_key, 'blue']
+		},
+
+		{
+			query: "SELECT id FROM alliance" +
+				" WHERE match_number = ?" +
+				"   AND event_code = ?" +
+				"   AND practice = ?" +
+				"   AND alliance_colour = ?",
+			args: [match.match_number, match.event_key, false, "blue"],
+			h: results => alliance_ids["blue"] = results[0].id
+		},
 
 		{
 			query: "INSERT INTO alliance_member (alliance_id, team_number)" +
@@ -101,9 +132,10 @@ function add_matches(connection, match) {
 				" ON DUPLICATE KEY UPDATE team_number = team_number;",
 			args: () => [alliance_ids["blue"],
 						 parseInt(match.alliances["blue"].team_keys[2].substring(3), 10)]
-		}
+		},
 
-
+		// done with this match, go on to the next one
+		() => load_match_list(connection, matches, done)
 
 	]);
 }
@@ -119,7 +151,7 @@ if (require.main === module) {
 	var event_code = process.argv[2];
     db.with_connection(connection =>  
 		       tba.matches_at_event(event_code,
-					    x => is_qualifying(x) && add_matches(connection, x),
-					    () => {}
+					    x => is_qualifying(x) && add_match(connection, x),
+					    () => after_all_matches_are_collected_load_them(connection)
 					   ));
 }
