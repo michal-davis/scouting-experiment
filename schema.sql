1c3c56c46dcfad63dede5a54dd4faf6f45df9377
Michal Davis <michalkdavis@gmail.com>
2019-07-10 19:55:37 -0400

    changed name of matches to frc_match and added match_id to frc_match and alliance (normalization)

--- schema.sql
+++ schema.sql
@@ -19,30 +19,30 @@ DROP TABLE IF EXISTS frc_event;
 CREATE TABLE frc_event (
 	event_code VARCHAR (32) PRIMARY KEY
 );
-DROP TABLE IF EXISTS matches;
-CREATE TABLE matches (
+DROP TABLE IF EXISTS frc_match;
+CREATE TABLE frc_match (
+	match_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	match_number NUMERIC, -- assigned by event scheduler (ex. quals 4)
     event_code VARCHAR (32),
     practice BOOLEAN DEFAULT FALSE,
-    PRIMARY KEY (match_number, event_code, practice),
-    CONSTRAINT FOREIGN KEY (event_code) REFERENCES frc_event (event_code) ON DELETE CASCADE
+    CONSTRAINT FOREIGN KEY (event_code) REFERENCES frc_event (event_code) ON DELETE CASCADE,
+    UNIQUE(match_number, event_code, practice)
 );
 DROP TABLE IF EXISTS alliance;
 CREATE TABLE alliance (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	match_number NUMERIC, 
-    event_code VARCHAR (32),
-    practice BOOLEAN DEFAULT FALSE,
+	alliance_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	match_id INT,
 	alliance_colour ENUM ('red', 'blue'), 
-    CONSTRAINT FOREIGN KEY (match_number, event_code, practice) REFERENCES matches (match_number, event_code, practice) ON DELETE CASCADE,
-    UNIQUE(match_number, event_code, practice, alliance_colour)
+    CONSTRAINT FOREIGN KEY (match_id) REFERENCES frc_match (match_id) ON DELETE CASCADE,
+    UNIQUE(match_id, alliance_colour)
 );
 DROP TABLE IF EXISTS alliance_member; 
 CREATE TABLE alliance_member (
 	alliance_id INT,
     team_number INT,
     CONSTRAINT FOREIGN KEY (team_number) REFERENCES team (team_number) ON DELETE CASCADE, 
-	CONSTRAINT FOREIGN KEY (alliance_id) REFERENCES alliance (id) ON DELETE CASCADE
+	CONSTRAINT FOREIGN KEY (alliance_id) REFERENCES alliance (alliance_id) ON DELETE CASCADE,
+    UNIQUE (alliance_id, team_number)
 );
 DROP TABLE IF EXISTS alliance_outcome;
 CREATE TABLE alliance_outcome (
@@ -50,8 +50,9 @@ CREATE TABLE alliance_outcome (
     score NUMERIC,
     RP1_rocket BOOLEAN,
     RP2_climbed BOOLEAN,
-    CONSTRAINT FOREIGN KEY (alliance_id) REFERENCES alliance (id) ON DELETE CASCADE
+    CONSTRAINT FOREIGN KEY (alliance_id) REFERENCES alliance (alliance_id) ON DELETE CASCADE
 );
+DROP TABLE IF EXISTS alliance_member_outcome;
 CREATE TABLE alliance_member_outcome (
 	alliance_id INT NOT NULL,
     team_number INT, 
@@ -63,7 +64,7 @@ CREATE TABLE alliance_member_outcome (
     end_level TINYINT,
     penalties INT,
     defense_time NUMERIC,
-    CONSTRAINT FOREIGN KEY (alliance_id) REFERENCES alliance (id) ON DELETE CASCADE,
+    CONSTRAINT FOREIGN KEY (alliance_id) REFERENCES alliance (alliance_id) ON DELETE CASCADE,
     CONSTRAINT FOREIGN KEY (team_number) REFERENCES alliance_member (team_number) ON DELETE CASCADE
     
 )
