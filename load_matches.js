1b8e641bbf80e1092895763e0c55118d4776f61d
Jim Davis <jrd3@alum.mit.edu>
2019-07-10 21:58:43 -0400

    Refactor load_matches to remove repetition and duplication from code

--- load_matches.js
+++ load_matches.js
@@ -18,22 +18,111 @@ function add_match(connection, match) {
 // this is called after TBA has found all the matches.
 function after_all_matches_are_collected_load_them(connection) {
     console.log("Loading " + matches.length + " matches");
-    load_match_list(connection, matches, () => 	connection.end());
+    load_match_list(connection, matches, () =>  connection.end());
 }
 
 // Load the list of matches, one at a time.
 // When all are loaded, call the done function.
 function load_match_list(connection, matches, done) {
     if (matches.length == 0) {
-	done();
-	return;
+        done();
+        return;
     }
     
     var match = matches.pop();
     console.log(match.event_key + " " + match.match_number);
     var alliance_ids = {"red": null, "blue": null};
     var match_id = null;
-    
+
+    function get_match_id() {
+        return match_id;
+    }
+
+    function get_team_number(colour, i) {
+        // team_keys always has frc prepended.  So strip the first three characters, then parse it as
+        // a base-10 number
+        return parseInt(match.alliances[colour].team_keys[i].substring(3), 10);
+    }
+
+    // Returns a function that can be used in an async series to insert the current match
+    // It also sets match_id (which is global with respect to this function) to the id of
+    // the match just inserted, so that subsequent queries can use it.
+    function get_match_inserter(match) {
+        return (cb) => 
+            connection.query(
+                "INSERT INTO frc_match (match_number, event_code, practice)" +
+                    " VALUES(?, ?, ?)" +
+                    " ON DUPLICATE KEY UPDATE event_code = event_code",
+                [match.match_number, match.event_key, false],
+                (error) => {
+                    if (error) {
+                        return cb(error);
+                    }
+                    // get the id of the row we just inserted
+                    connection.query(
+                        "SELECT match_id FROM frc_match" +
+                            " WHERE match_number = ?"+
+                            "   AND event_code = ?" +
+                            "   AND practice = ?",
+                        [match.match_number, match.event_key, false],
+                        (error, results) => {
+                            match_id = results[0].match_id;
+                            cb(error);
+                        }
+                    );
+                    }
+                );
+    }
+
+    // Returns a function that can be used in an async series
+    // to insert an alliance using the current match_id for a given colour.
+    // It also fetches the id the newly inserted alliance and stores it in
+    // alliance_ids for use by team inserters.
+    // An asycn series function takes one argument (cb) which it calls whenever it needs to exit, whether with error
+    // or not.
+    function get_alliance_inserter(colour) {
+        return (cb) => {
+            // we do not know the match_id at the time this is called, so the inserter function has
+            // to call a function at run time to get it
+            var match_id = get_match_id();
+            connection.query(
+            "INSERT INTO alliance (match_id, alliance_colour)" +
+                " VALUES (?, ?)" + 
+                "ON DUPLICATE KEY UPDATE match_id = match_id",
+                [match_id, colour],
+                (error, results) => {
+                    if (error) {
+                        return cb(error);
+                    }
+                    connection.query(
+                        "SELECT alliance_id FROM alliance " +
+                            " WHERE match_id = ? AND alliance_colour = ?",
+                        [match_id, colour],
+                        (error, results) => {
+                            if (error) {
+                                return cb(error);
+                            }
+                            alliance_ids[colour] = results[0].alliance_id;
+                            cb(error);
+                        });
+                }
+            );
+        };
+    }
+
+    function get_team_inserter(colour, i) {
+        return (cb) => {
+            var alliance_id = alliance_ids[colour];
+            connection.query(
+                "INSERT INTO alliance_member (alliance_id, team_number)" +
+                    " VALUES (?, ?) " +
+                    " ON DUPLICATE KEY UPDATE team_number = team_number;",
+                [alliance_id, get_team_number(colour, i)],
+                (error, results) => cb(error)
+            );
+        };
+    }
+
     // Node runs each database query asynchronously, but we want to wait
     // for (some of the) queries to finish before doing the next one so we
     // can use results from previous queries.  To do this we need to use
@@ -43,131 +132,24 @@ function load_match_list(connection, matches, done) {
     // calls the next operation (unless there is an error)
 
     async.series(
-	[
-	    
-	    // insert match
-	    (cb) => connection.query(
-		"INSERT INTO frc_match (match_number, event_code, practice)" +
-                    " VALUES(?, ?, FALSE)" +
-                    " ON DUPLICATE KEY UPDATE event_code = event_code",
-		[match.match_number, match.event_key],
-		(error, results) => cb(error)
-	    ),
+    [
+        get_match_inserter(match),
+        get_alliance_inserter("red"),
+        get_team_inserter("red", 0),
+        get_team_inserter("red", 1),
+        get_team_inserter("red", 2),
 
-	    (cb) => connection.query(
-		"SELECT match_id FROM frc_match" +
-		    " WHERE match_number = ?"+
-		    "   AND event_code = ?" +
-		    "   AND practice = ?",
-		[match.match_number, match.event_key, false],
-		(error, results) => {
-		    match_id = results[0].match_id;
-		    cb(error);
-		}
-	    ),
-	    
-	    // insert red alliance
-	    (cb) => connection.query(
-		"INSERT INTO alliance (match_id, alliance_colour)" +
-                    " VALUES (?, ?)" + 
-                    "ON DUPLICATE KEY UPDATE match_id = match_id",
-		[match_id, 'red'],
-		(error, results) => cb(error)
-	    ),
-	    
-	    // get id of red alliance
-	    (cb) => connection.query(
-		"SELECT alliance_id FROM alliance " +
-		    " WHERE match_id = ?" +
-		    "   AND alliance_colour = ?",
-		[match_id, 'red'],
-		(error, results) => {
-		    alliance_ids["red"] = results[0].alliance_id;
-		    cb(error);
-		}
-	    ),
-	    
-	    // insert the red members 0,1,2
-	    (cb) => connection.query(
-		"INSERT INTO alliance_member (alliance_id, team_number)" +
-		    " VALUES (?, ?) " +
-		    " ON DUPLICATE KEY UPDATE team_number = team_number;",
-		[alliance_ids["red"],
-		 parseInt(match.alliances["red"].team_keys[0].substring(3), 10)],
-		(error, results) => cb(error)
-	    ),
-	    (cb) => connection.query(
-		"INSERT INTO alliance_member (alliance_id, team_number)" +
-		    " VALUES (?, ?) " +
-		    " ON DUPLICATE KEY UPDATE team_number = team_number;",
-		[alliance_ids["red"],
-		 parseInt(match.alliances["red"].team_keys[1].substring(3), 10)],
-		(error, results) => cb(error)
-	    ),
-	    
-	    (cb) => connection.query(
-		"INSERT INTO alliance_member (alliance_id, team_number)" +
-		    " VALUES (?, ?) " +
-		    " ON DUPLICATE KEY UPDATE team_number = team_number;",
-		[alliance_ids["red"],
-		 parseInt(match.alliances["red"].team_keys[2].substring(3), 10)],
-		(error, results) => cb(error)
-	    ),
-	    
-	    // insert the blue alliance 
-	    (cb) => connection.query(
-		"INSERT INTO alliance (match_id, alliance_colour)" +
-                    " VALUES (?, ?)" + 
-		    " ON DUPLICATE KEY UPDATE match_id = match_id",
-		[match_id, 'blue'],
-		(error, results) => cb(error)
-	    ),
-	    
-	    //  get the alliance id
-	    (cb) => connection.query(
-		"SELECT alliance_id FROM alliance" +
-		    " WHERE match_id = ?" +
-		    "   AND alliance_colour = ?",
-		[match_id, 'blue'],
-		(error, results) => {
-		    alliance_ids["blue"] = results[0].alliance_id;
-		    cb(error);
-		}
-	    ),
-	    
-	    // insert teams 
-	    (cb) => connection.query(
-		"INSERT INTO alliance_member (alliance_id, team_number)" +
-		    " VALUES (?, ?) " +
-		    " ON DUPLICATE KEY UPDATE team_number = team_number;",
-		[alliance_ids["blue"],
-		 parseInt(match.alliances["blue"].team_keys[0].substring(3), 10)],
-		(error, results) => cb(error)
-	    ),
-	    
-	    (cb) => connection.query(
-		"INSERT INTO alliance_member (alliance_id, team_number)" +
-		    " VALUES (?, ?) " +
-		    " ON DUPLICATE KEY UPDATE team_number = team_number;",
-		[alliance_ids["blue"],
-		 parseInt(match.alliances["blue"].team_keys[1].substring(3), 10)],
-		(error, results) => cb(error)
-	    ),
-	    
-	    (cb) => connection.query(
-		"INSERT INTO alliance_member (alliance_id, team_number)" +
-		    " VALUES (?, ?) " +
-		    " ON DUPLICATE KEY UPDATE team_number = team_number;",
-		[alliance_ids["blue"],
-		 parseInt(match.alliances["blue"].team_keys[2].substring(3), 10)],
-		(error, results) => cb(error)
-	    )
-	],
-	
-	// This is called when all are finished
-	(err, results) => {
-	    load_match_list(connection, matches, done)
-	}
+        get_alliance_inserter("blue"),
+        get_team_inserter("blue", 0),
+        get_team_inserter("blue", 1),
+        get_team_inserter("blue", 2)
+
+    ],
+    
+    // This is called when all are finished
+    (err, results) => {
+        load_match_list(connection, matches, done)
+    }
     );
     
 }
@@ -178,12 +160,12 @@ function is_qualifying(x){
 
 if (require.main === module) {
     if (process.argv.length < 3) {
-	throw "Missing argument: event_code";
+    throw "Missing argument: event_code";
     }
     var event_code = process.argv[2];
     db.with_connection(connection =>  
-		       tba.matches_at_event(event_code,
-					    x => is_qualifying(x) && add_match(connection, x),
-					    () => after_all_matches_are_collected_load_them(connection)
-					   ));
+               tba.matches_at_event(event_code,
+                        x => is_qualifying(x) && add_match(connection, x),
+                        () => after_all_matches_are_collected_load_them(connection)
+                       ));
 }
